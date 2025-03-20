d6fc74c635b53626f14aa3283b2bd3dcd962adc1
SomisCreations <89362410+samriddhighosh@users.noreply.github.com>
2025-03-20 13:44:31 -0500

    revise

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Card = ({title,description,img, button}:{title?:string, description?:string, button?:string}) => {
+const Card = ({title,description, button}:{title?:string, description?:string, button?:string}) => {
   return (
     <div className='bg-gray-50 pl-8 py-6 pr-14 rounded-3xl items-start text-left w-1/2'>
-        {img&&<img src={img} alt="top-image"/>}
+        {/* {img&&<img src={img} alt="top-image"/>} */}
         <h2 className='heading text-[28px] !font-openSans font-bold'>{title}</h2>
         <p className='des text-neutral-600'>{description}</p>
         {button &&<button>{button}</button>}
