52a9af6f9d11b8e001b7cbfd1c6ba3ead68643f2
SomisCreations <89362410+samriddhighosh@users.noreply.github.com>
2025-03-23 18:21:45 -0500

    yes

--- components/Division.tsx
+++ components/Division.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Division = ({title,description, button, icon, className}:{title:string, description:string, button?:string, className?:string}) => {
+const Division = ({title,description, button, icon, className}:{title:string, description:string, button?:string, className?:string,icon?: React.ReactNode;}) => {
   return (
     <div className={`py-6 items-start text-left w-1/2 border-r-2 ${className}`}>
-       {
-        icon && {icon}
-       }
+       {icon && icon} 
         <h2 className='heading text-[32px] !font-openSans font-bold'>{title}</h2>
         <p className='des text-neutral-600'>{description}</p>
         {button &&<button className='pt-2'>{button}</button>}
